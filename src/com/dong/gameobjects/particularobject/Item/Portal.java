5b2e12e7b52f88673d8200324bc76ae9ab17a1f4
Bui Huy Dong <huydong0210@gmail.com>
2021-12-07 23:52:35 +0700

    item

--- src/com/dong/gameobjects/particularobject/Item/Portal.java
+++ src/com/dong/gameobjects/particularobject/Item/Portal.java
@@ -13,23 +13,22 @@ import java.awt.*;
 
 public class Portal extends Item {
     Animation aniPortal;
+
     public Portal(float posX, float posY, float width, float height, GameWorld gameWorld) {
         super(posX, posY, width, height, gameWorld);
-        aniPortal= CacheDataLoader.getInstance().getAnimation(AnimationName.portal);
+        aniPortal = CacheDataLoader.getInstance().getAnimation(AnimationName.portal);
     }
 
     @Override
-    public void update(){
+    public void update() {
         if (isCheckUsed() == false) {
             ParticularObject object = getGameWorld().haveSamePosition(this);
             if (object instanceof Brick)
                 setState(ALIVE);
-            if (getState()==ALIVE)
-            {
-                Rectangle rectItem=new Rectangle((int ) getPosX(),(int )getPosY(),(int) getWidth(), (int ) getHeight());
-                ParticularObject object1=getGameWorld().getCollisionWithRectangle(rectItem);
-                if (object1 instanceof Player && getGameWorld().countEnemy()==0)
-                {
+            if (getState() == ALIVE) {
+                Rectangle rectItem = new Rectangle((int) getPosX(), (int) getPosY(), (int) getWidth(), (int) getHeight());
+                ParticularObject object1 = getGameWorld().getCollisionWithRectangle(rectItem);
+                if (object1 instanceof Player && getGameWorld().countEnemy() == 0) {
                     clip.start();
                     setCheckUsed(true);
                     setState(DEATH);
@@ -38,20 +37,24 @@ public class Portal extends Item {
             }
         }
 
-        if (isCheckUsed()==true && getGameWorld().countEnemy()==0)
-        {
-           getGameWorld().setNextLevel(true);
-           setCheckUsed(false);
-           getGameWorld().backGround.setState(BackGround.WIN_LEVEL);
+        if (isCheckUsed() == true && getGameWorld().countEnemy() == 0) {
+            if (CacheDataLoader.getInstance().getLevel() == BackGround.FINAL_LEVEL) {
+                getGameWorld().getBackGround().setState(BackGround.WIN_GAME);
+            } else {
+                getGameWorld().setNextLevel(true);
+                setCheckUsed(false);
+                getGameWorld().remove(getGameWorld().player);
+                getGameWorld().backGround.setState(BackGround.WIN_LEVEL);
+                CacheDataLoader.getInstance().loadLevel();
+            }
         }
     }
 
     @Override
     public void draw(Graphics2D g2) {
-        if (this.getState()==ALIVE)
-        {
+        if (this.getState() == ALIVE) {
             aniPortal.update(System.nanoTime());
-            aniPortal.draw((int)getPosX(),(int) getPosY(),g2);
+            aniPortal.draw((int) getPosX(), (int) getPosY(), g2);
         }
     }
 }
